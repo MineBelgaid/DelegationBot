90375487a7d28d2a7f6c1daa83a96670bca40249
Mine.belgaid <amine.a.belgaid@gmail.com>
2021-10-22 14:45:34 +0100

    added voice

--- main.py
+++ main.py
@@ -15,7 +15,14 @@ from discord.ext import commands, tasks
 from keep_alive import keep_alive
 from PIL import Image, ImageDraw, ImageFilter, ImageOps, ImageDraw
 
-
+if not discord.opus.is_loaded():
+    # the 'opus' library here is opus.dll on windows
+    # or libopus.so on linux in the current directory
+    # you should replace this with the location the
+    # opus library is located in and with the proper filename.
+    # note that on windows this DLL is automatically provided for you
+    discord.opus.load_opus('opus')
+    
 with open('./assets/secrets.json', 'r') as f:
     secrets = json.load(f)
 with open('./assets/data.json', 'r') as k:
